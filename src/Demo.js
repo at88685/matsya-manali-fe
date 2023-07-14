f8a5b26e79bb94d769e919b7ed132b26ccb2f6a5
at88685 <79696635+at88685@users.noreply.github.com>
2023-07-14 23:25:31 +0530

    setup

---
+++ src/Demo.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const Demo = () => {
+  return (
+    <>
+    <Button variant="outlined">
+      Matsya Manali
+    </Button>
+    <Box>
+        <Typography variant="h2">Matsya Manali</Typography>
+    </Box>
+    
+    
+    </>
+    
+  )
+}
+
+export default Demo
