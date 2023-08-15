ba6df7d7c197a518eb7062a3d9e81e941cbc5a06
Escape <Escape>
2023-08-16 00:54:31 +0530

    carousel added to homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,32 @@
-import { useEffect, useState } from 'react';
-import { Button } from '@mui/material';
-import frontVideo from '../assets/video/frontVideo.mp4';
+import React, { useEffect, useState } from "react";
+import Carousel from "react-material-ui-carousel";
+import { Box, Button, styled } from "@mui/material";
+import { InsideImagesList } from "../Constants/ImagesList";
 import './Styles/home.css'
 import { motion } from "framer-motion";
 
+const TransparentOverlay = styled("div")({
+  position: "absolute",
+  top: 0,
+  left: 0,
+  width: "100%",
+  height: "100%",
+  background: "rgba(0, 0, 0, 0.5)",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
 
-import React from 'react'
+const Text = styled("div")({
+  color: "#fff", 
+  textAlign: "center",
+  padding: "16px",
+});
+
 const Home = () => {
+
+  const imagesList = InsideImagesList
+
   const openVirtualSite = () => {
     window.open("https://dotcomsolutions.in/matsya-manali/", "_black", "noreferrer");
   }
@@ -17,28 +37,42 @@ const Home = () => {
 
   const [showMainContent, setShowMainContent] = useState(false);
   const deluxFadeDownVariants = {
-    hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0 }
+    hidden: { opacity: 0, y: 0 },
+    visible: { opacity: 1, y: -200 }
   };
-
   return (
-    <div className="homeMainDiv" id='homeid'>
-      <video autoPlay muted loop style={{ objectFit: 'cover', height: '100vh', width: '100%', opacity: '1' }}>
-        {/* <source src={frontVideo} type="video/mp4" /> */}
-        {/* Add additional <source> elements for different video formats (e.g., WebM, Ogg) */}
-        {/* Fallback content for browsers that don't support HTML5 video */}
-        <source src={frontVideo} type='video/mp4' />
-      </video>
-      {/* <div className='home-body'> */}
-      <motion.div className='welcomeToMatsyaManaliDiv'
+    <Box position="relative" height="100vh" overflow="hidden" id='homeid'>
+      <Carousel
+        animation="fade"
+        navButtonsAlwaysVisible={false}
+        autoPlay={true}
+        
+      >
+        {imagesList.map((item, index) => (
+          <div key={index} style={{ position: "relative" }}>
+            <img
+              src={item}
+              alt="insideview"
+              style={{
+                objectFit: "cover",
+                width: "100%",
+                height: "100vh",
+              }}
+            />
+
+            {/* Transparent overlay with centered text */}
+            <TransparentOverlay>
+            <motion.div className='welcomeToMatsyaManaliDiv'
         initial="hidden"
         animate={showMainContent ? "visible" : "hidden"}
         variants={deluxFadeDownVariants}
-        transition={{ duration: 1.5 }}
+        transition={{ duration: 2 }}
 
       >
+        <Text>
         <div className='welcomeToMatsyaManaliText'>Welcome to Matsya Manali</div>
         <div className='discoverGreatnessText'>Discover the greatness of mountains with an eccentric feel of nature.</div>
+        </Text>
         <Button variant='outlined' sx={{
           color: 'white',
           ":hover": {
@@ -48,9 +82,12 @@ const Home = () => {
           marginTop: '15px'
         }} onClick={openVirtualSite}>Explore Virtually</Button>
       </motion.div>
-    </div>
-    // </div>
-  )
-}
+            </TransparentOverlay>
+          </div>
+        ))}
+      </Carousel>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
